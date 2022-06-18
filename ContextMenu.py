97be50fcc2af6cc16d039c5b84519f324492c164
Loki hornsby <lokihornsby@gmail.com>
2022-06-18 21:57:53 +0100

    Updated icos and added very buggy cell swapping

--- ContextMenu.py
+++ ContextMenu.py
@@ -1,3 +1,4 @@
+from pydoc import ispath
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
@@ -70,53 +71,59 @@ def Start(self):
         f.write(date + "\n")
         f.write(len(date)*"_" + "\n")
         f.write("\n")
+
         f.close()
 
     def ViewChangelog(path):
         os.system('"' + path + '"')
 
     if User.Data.StoredKeys[self.currentColumn()] == "location": # Location
-        # Menu
-        self.contextMenu = QMenu(self)
+        # Only open menu if path is valid
+        if isdir(Folder.GetPath()):
+            # Menu
+            self.contextMenu = QMenu(self)
 
-        # Edit path on item
-        Edit = QAction("Edit Path", self)
-        Edit.triggered.connect(lambda: EditDir())
-        Edit.setIcon(QIcon("Images/Edit.ico"))
-        self.contextMenu.addAction(Edit)
+            # Edit path on item
+            Edit = QAction("Edit Path", self)
+            Edit.triggered.connect(lambda: EditDir())
+            Edit.setIcon(QIcon("Images/Edit.ico"))
+            self.contextMenu.addAction(Edit)
 
-        # Create changelog with todays date
-        # TODO: ** I may make this into a custom file with a header / metadata one day
-        # show only if file doesn't already exist
-        if not isfile(Folder.FileToday()):
-            CreateOpen = QAction("Create " + Folder.GetDate("%d.%m.%y.txt"), self)
-            CreateOpen.setIcon(QIcon("Images/Create.ico"))
-            CreateOpen.triggered.connect(lambda: CreateChangelog())
+            # Create changelog with todays date
+            # TODO: ** I may make this into a custom file with a header / metadata one day
+            # show only if file doesn't already exist
+            if not isfile(Folder.FileToday()):
+                CreateOpen = QAction("Create " + Folder.GetDate("%d.%m.%y.txt"), self)
+                CreateOpen.setIcon(QIcon("Images/Create.ico"))
+                CreateOpen.triggered.connect(lambda: CreateChangelog())
+            else:
+                CreateOpen = QAction("Open "  + Folder.GetDate("%d.%m.%y.txt"), self)
+                CreateOpen.setIcon(QIcon("Images/Open.ico"))
+                CreateOpen.triggered.connect(lambda: ViewChangelog(Folder.GetPath() + Folder.GetDate("%d.%m.%y.txt")))
+            
+            self.contextMenu.addAction(CreateOpen)
+
+            # View previous changelog
+            # show only if there is more than 1 file
+            if len(Folder.allfiles) > 1:
+                View = QAction("View last changelog", self)
+                View.triggered.connect(lambda: ViewChangelog(Folder.FileLast()))
+                View.setIcon(QIcon("Images/View.ico"))
+                self.contextMenu.addAction(View)
+
+            # TODO: MERGELOG
+            # TODO: TIDY UP CODE (SPLI IN DIFF FILES)
+            # TODO: Seperate window to view changelogs
+            # Merge files into one single file  
+            Merge = QAction("Update mergelog", self)
+            Merge.triggered.connect(lambda: print("Merge all cahngelogs into a single file"))
+            Merge.setIcon(QIcon("Images/Merge.ico"))
+            self.contextMenu.addAction(Merge)
+
+            # Show Menu
+            self.contextMenu.popup(QCursor.pos())
         else:
-            CreateOpen = QAction("Open "  + Folder.GetDate("%d.%m.%y.txt"), self)
-            CreateOpen.setIcon(QIcon("Images/Open.ico"))
-            CreateOpen.triggered.connect(lambda: ViewChangelog(Folder.GetPath() + Folder.GetDate("%d.%m.%y.txt")))
-        
-        self.contextMenu.addAction(CreateOpen)
-
-        # View previous changelog
-        # show only if there is more than 1 file
-        if len(Folder.allfiles) > 1:
-            View = QAction("View last changelog", self)
-            View.triggered.connect(lambda: ViewChangelog(Folder.FileLast()))
-            View.setIcon(QIcon("Images/View.ico"))
-            self.contextMenu.addAction(View)
-
-        # TODO: MERGELOG
-        # TODO: TIDY UP CODE (SPLI IN DIFF FILES)
-        # TODO: Seperate window to view changelogs
-        # Merge files into one single file  
-        Merge = QAction("Update mergelog", self)
-        Merge.triggered.connect(lambda: print("Merge all cahngelogs into a single file"))
-        Merge.setIcon(QIcon("Images/Merge.ico"))
-        self.contextMenu.addAction(Merge)
-
-        # Show Menu
-        self.contextMenu.popup(QCursor.pos())
+            self.editItem(self.currentItem())
+            EditDir()
     else:
         self.editItem(self.currentItem())
